bdff6feea78b23dc2d45933e2a0f1a5abf60a8e1
dariashid <dariashid@mail.ru>
2024-02-25 15:14:02 +0300

    OOP2

---
+++ src/MotorVehicle.java
@@ -0,0 +1,14 @@
+public abstract class MotorVehicle extends Vehicle {
+    public MotorVehicle(String modelName, int wheelsCount){
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine(){
+        System.out.println("Проверяем двигатель.");
+    }
+
+    @Override
+    public void check() {
+        super.check();
+        checkEngine();
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,14 @@
+public class Truck extends MotorVehicle{
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkTrailer(){
+        System.out.println("Проверяем прицеп.");
+    }
+
+    @Override
+    public void check() {
+        super.check();
+        checkTrailer();
+    }
+}
